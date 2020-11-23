214a237f335194ef0f71536c1dc90db553df13f9
Edmund Dong <12579441+EdmundDong@users.noreply.github.com>
2020-11-22 20:29:06 -0700

    Added new routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
 from app import flaskapp
 
 @flaskapp.route('/')
-@flaskapp.route('/index')
+@flaskapp.route('/index/')
 def index():
     return "Hello World!"
+
+@flaskapp.route('/popular/')
+def popular():
+    return "List of top games"
+
+@flaskapp.route('/search/')
+def search():
+    return "Search for games here"
+
+@flaskapp.route('/profile/')
+def game(game_id):
+    return "Profile page"
+
+@flaskapp.route('/game/<int:game_id>/')
+def game(game_id):
+    return "Page for game #" + str(game_id)
