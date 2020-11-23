7dede34538db8f9a4bf850c5fc17ceabb288cf3e
Edmund Dong <12579441+EdmundDong@users.noreply.github.com>
2020-11-22 22:11:30 -0700

    updated database.py

--- app/database.py
+++ app/database.py
@@ -6,9 +6,14 @@ class db():
     def __init__(self):
         self.connection = None
         try:
-            self.connection = psycopg2.connect(database = "eedong")
-            #self.connection = psycopg2.connect(host = "localhost", port = "8088", database = "GameDB")
-
+            try:
+                self.connection = psycopg2.connect(database = "eedong")
+            except:
+                try:
+                    self.connection = psycopg2.connect(host = "localhost", port = "8088", database = "GameDB")
+                except: 
+                    print("Error connection with database")
+                    exit(0)
         except:
             print("Error connection with database")
             exit(0)
