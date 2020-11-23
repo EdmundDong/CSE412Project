da799e986d8982d1c2d2271fd8c771cbd3c56039
Ricky Hsu <ricky.h.hsu@gmail.com>
2020-11-22 20:34:07 -0700

    some template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import flaskapp
+from flask import request, render_template
+
 
 @flaskapp.route('/')
 @flaskapp.route('/index')
 def index():
     return "Hello World!"
+
+@flaskapp.route('/login', methods=["GET"])
+def login():
+    return render_template()
